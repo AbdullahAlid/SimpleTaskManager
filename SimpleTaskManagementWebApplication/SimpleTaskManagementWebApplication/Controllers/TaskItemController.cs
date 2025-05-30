93accc9286bfcb2570b96706a7920c5d2ac9911b
Abdullah Alid <alidabdullah404@gmail.com>
2025-05-31 01:31:59 +0600

    Initial Setup With some basic work

---
+++ SimpleTaskManagementWebApplication/SimpleTaskManagementWebApplication/Controllers/TaskItemController.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleTaskManagementWebApplication.Data;
+using SimpleTaskManagementWebApplication.Models;
+
+namespace SimpleTaskManagementWebApplication.Controllers
+{
+    public class TaskItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public TaskItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var tasks = await _context.TaskItems.ToListAsync();
+            return View(tasks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Title, Description, DueDate")] TaskItem task)
+        {
+            return View(task);
+        }
+
+
+    }
+}
---
+++ SimpleTaskManagementWebApplication/SimpleTaskManagementWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleTaskManagementWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
